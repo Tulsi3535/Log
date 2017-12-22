b7ace6315d91ddefe15950563df2aec21724979f
hiren <patelhiren3535@gmail.com>
2017-12-22 13:34:58 +0530

    Log Added..!

--- Log_Example/app/src/main/java/tulsi/log_example/MainActivity.java
+++ Log_Example/app/src/main/java/tulsi/log_example/MainActivity.java
@@ -1,13 +1,37 @@
 package tulsi.log_example;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText name;
+    Button btn;
+    private  String TAG = "MainActivity Log >";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.editText);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Button Clicked...!!");
+                if (!name.getText().toString().isEmpty()) {
+                    Log.d(TAG, "Entered Name = " + name.getText().toString());
+                }else {
+                    Log.e(TAG, "Name is Empty...!!");
+                }
+            }
+        });
+
     }
 }
